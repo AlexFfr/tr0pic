71e95dc8f1b3b89177606d951915c38d8a5c16be
Alex <31145882+AlexFfr@users.noreply.github.com>
2024-02-04 15:44:08 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.fade-in-on-load').hide().fadeIn(2000);
+});
+
+// function () {
+//     $('.fade-in-on-load-2').hide();
+// };
+
+// function fadeIn2() {
+//     $('.fade-in-on-load-2').hide().fadeIn(2000);
+// };
+
+// setTimeout(fadeIn2, 2000);
